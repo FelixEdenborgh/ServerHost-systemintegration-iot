26ba134b649485a20f55520327058bb659faaadc
ErrorX <31070311+Nalox1@users.noreply.github.com>
2019-10-30 13:23:38 +0100

    update

--- ServerHallHost/src/java/javafiles/ElKostnad.java
+++ ServerHallHost/src/java/javafiles/ElKostnad.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "elkostnadtabel")
+@XmlRootElement(name = "elkostnadtable")
 public class ElKostnad implements Serializable
 {
 
